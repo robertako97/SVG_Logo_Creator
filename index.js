4e89d161dd6c39949def2785b46a0e2baeff27a3
robertako97 <roberto.diazgmx@gmail.com>
2023-08-15 20:14:48 -0700

    Finished logo render, need tests and constrictions

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-
+//WriteFile
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error('Error writing to file:', err);
+        } else {
+            console.log(`Data has been written to ${fileName}`);
+        }
+    });
+}
 //User Prompts
 async function getUserInputs() {
     const answers = await inquirer.prompt([
@@ -11,74 +20,66 @@ async function getUserInputs() {
                 'Square',
                 'Triangle',
                ],
-            name: 'thisshape'
+            name: 'shape'
         },
         {
             type: 'input',
             message: "Which are your logo lettermarks?",
-            name: 'thistext',
+            name: 'text',
         },
         {
             type: 'input',
             message: "What color would you like the shape of your logo? You can define a color or a hexadecimal color code!",
-            name: 'thiscolorShape',
+            name: 'colorShape',
         },
         {
             type: 'input',
             message: "What color would you like the shape of your text? You can define a color or a hexadecimal color code!",
-            name: 'thiscolorText',
+            name: 'colorText',
         }
     ]);
 
     return answers;
 }
 //Function depending on userShape
-async function constructorFunc() {
+async function constructorFunc(userInput) {
+    let SVG_Content = '';
 
-if (userInput.shape == 'Circle') {
-    SVG_Circle()
-    {
-        return `
-        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="150" cy="100" r="80" fill="${this.colorShape}" />
-            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.colorText}"> ${this.text} </text>
-        </svg>
-        `
-    }
-} else if (userInput.shape == 'Triangle') {
-    SVG_Triangle()
-    {
-        return `
-        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-            <polygon points="150,5 280,180 20,180" fill="${this.colorShape}" />
-            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.colorText}"> ${this.text} </text>
-        </svg>
-        `
+    if (userInput.shape === 'Circle') {
+        SVG_Content =
+            `
+            <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="150" cy="100" r="80" fill="${userInput.colorShape}" />
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="${userInput.colorText}"> ${userInput.text} </text>
+            </svg>
+            `;
+    } else if (userInput.shape === 'Triangle') {
+        SVG_Content =
+            `
+            <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="150,5 280,180 20,180" fill="${userInput.colorShape}" />
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="${userInput.colorText}"> ${userInput.text} </text>
+            </svg>
+            `;
+    } else if (userInput.shape === 'Square') {
+        SVG_Content =
+            `
+            <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                <rect x="50" y="50" width="200" height="200" fill="${userInput.colorShape}" />
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="${userInput.colorText}"> ${userInput.text} </text>
+            </svg>
+            `;
 
-    }
-} else if (userInput.shape == 'Square') {
-
-    SVG_Square()
-    {
-        return `
-        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-            <rect x="50" y="50" width="200" height="200" fill="${this.colorShape}" />
-            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.colorText}"> ${this.text} </text>
-        </svg>
-        `
-
-    }
 }
+    const fileName = 'logo.SVG'
+    writeToFile(fileName, SVG_Content);
 }
 
-//Constructor to call functions for SVG
-
+//Main Func
 async function init (){
 
     try{
         const userInput = await getUserInputs();
-        const SVGInput = await constructorFunc(userInput.thisshape);
-
         class logo {
             constructor(shape, colorShape, text, colorText) {
                 this.shape = shape;
@@ -87,11 +88,14 @@ async function init (){
                 this.colorText = colorText;
             }
 
+            file (){
+                constructorFunc(userInput);
+            }
+
         }
 // UserInput to make constructor
-        const constructorInput = new logo( userInput.thisshape, userInput.thiscolorShape, userInput.thistext, userInput.thiscolorText);
-
-
+        const constructorInput = new logo( userInput.shape, userInput.colorShape, userInput.text, userInput.colorText);
+        constructorInput.file();
 
     } catch (error) {
     if (error.isTtyError) {
